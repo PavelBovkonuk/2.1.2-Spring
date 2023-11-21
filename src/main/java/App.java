acb8bca8795faee01cfea8f7498ec0b81a495304
Pavel Bovkonuk <pavelbovkonuk@gmail.com>
2023-11-21 17:32:02 +0300

    versoin 1.0

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,13 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        Cat beanCat1 = (Cat) applicationContext.getBean("cat");
+        Cat beanCat2 = (Cat) applicationContext.getBean("cat");
+
+
+        System.out.println("helloworld comparison result: " + bean.equals(bean2));
+        System.out.println("cat comparison result: " + beanCat1.equals(beanCat2));
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,17 @@
+import java.util.Objects;
+
+public class Cat {
+
+    private String name;
+
+    Cat() {
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
